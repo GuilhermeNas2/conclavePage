669526205f1a22c927d197a7dcd876e3b8dde748
NoobwanX9 <tkdhouse2@gmail.com>
2023-07-08 23:17:20 -0300

    finally commit

--- javaScript/darkMode.js
+++ javaScript/darkMode.js
@@ -5,12 +5,21 @@ const container = document.querySelector('.container');
 function toggleMode() {   
 
     if (container.classList.contains('dark')) {
+        
         container.classList.remove('dark');
+
+        button.classList.remove('slidebutton');
+        button.classList.add('slide-back');
         button.style.background = ('white var(--switch-bg-url) no-repeat center');
+
     } else {
+        
         container.classList.add('dark');
 
+        button.classList.add('slidebutton');
+        button.classList.remove('slide-back')
         button.style.background = ('white var(--switch-bg-moon) no-repeat center');
+        
     }
     
 }
